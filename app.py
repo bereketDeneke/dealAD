f458e1b466761357e56ce687c39d911a14180568
Absera <abseratemesgen@gmai.com>
2022-11-12 14:11:37 +0400

    123

--- app.py
+++ app.py
@@ -6,28 +6,29 @@ DOMAIN = "dealAD"
 
 @app.route('/create', methods=['GET'])
 def create_post():
-    return render_template("create_post.html")
+    return render_template("posts/create_post.html")
 @app.route('/create/sell', methods=['GET'])
 def create_sell():
-    return render_template("create_sell.html")
+    return render_template("posts/create_sell.html")
 
 @app.route('/create/sell', methods=['GET', 'POST'])
 def create_sell_action():
-    return render_template("create_sell.html")
+    return render_template("posts/create_sell.html")
 @app.route('/create/buy', methods=['GET'])
 def create_buy():
-    return render_template("create_buy.html")
+    return render_template("posts/create_buy.html")
 
 @app.route('/create/buy', methods=['GET', 'POST'])
 def create_buy_action():
-    return render_template("create_buy.html")
-@app.route(f'{DOMAIN}/createSell/', methods=['GET'])
-def getMembers(request):
-    return render_template("createsellPost.html", request)
+    return render_template("posts/create_buy.html")
 
-@app.route('/api/v1/register', methods=['POST'])
-def register(request):
-    validate.validateInput(request)
+# @app.route(f'{DOMAIN}/createSell/', methods=['GET'])
+# def getMembers(request):
+#     return render_template("createsellPost.html", request)
+
+# @app.route('/api/v1/register', methods=['POST'])
+# def register(request):
+#     validate.validateInput(request)
 
 app.run(debug=True)
 
