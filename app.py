84acf9f0604e20d16100200594dcdaecef66404e
Absera <abseratemesgen@gmai.com>
2022-11-12 17:01:29 +0400

    123

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import jsonify, render_template, request
 
 app = flask.Flask(__name__)
 
+INIT()
 @app.route('/', methods=['GET'])
 def login():
     if request.method == 'POST':
@@ -24,14 +25,12 @@ def myPosts():
 #  =================================================================
 @app.route('/market/buy')
 def market_buy():
-    buy_posts = getDB.execute("SELECT * FROM buy_posts").fetchall()
-    print(buy_posts)
+    buy_posts = getDB()[1].execute("SELECT * FROM buy_posts").fetchall()
     return render_template("posts/browse_buy.html", buy_posts=buy_posts)
 
 @app.route('/market/sell')
 def market_sell():
-    sell_posts = getDB.execute("SELECT * FROM sell_posts").fetchall()
-    print(sell_posts)
+    sell_posts = getDB()[1].execute("SELECT * FROM sell_posts").fetchall()
     return render_template("posts/browse_sell.html", sell_posts=sell_posts)
 
 @app.route('/create', methods=['GET'])
@@ -48,11 +47,12 @@ def create_sell_action():
         user_name = "ay2395"
         amount = request.form.get("amount")
         rate = request.form.get("rate")
-        database_cursor.execute(''' INSERT INTO sell_posts ( user_name, amount, rate )
+        getDB()[0].execute(''' INSERT INTO sell_posts ( user_name, amount, rate )
                                VALUES ( ?, ?,
                                ?); ''', (user_name, amount, rate))
-        database.commit()
+        getDB()[1].commit()
         return render_template("posts/create_sell.html")
+    return render_template("posts/create_sell.html")
 
 @app.route('/create/buy', methods=['GET'])
 def create_buy():
@@ -64,11 +64,12 @@ def create_buy_action():
         user_name = "temp"#todo
         amount = request.form.get("amount")
         rate = request.form.get("rate")
-        database_cursor.execute(''' INSERT INTO buy_posts ( user_name, amount, rate )
+        getDB()[1].execute(''' INSERT INTO buy_posts ( user_name, amount, rate )
                                VALUES ( ?, ?,
                                ?); ''', (user_name, amount, rate))
-        database.commit()
+        getDB()[0].commit()
         return render_template("posts/create_buy.html")
+    return render_template("posts/create_sell.html")
 
-app.run(debug=True, port=80)
+app.run(debug=True)
 
