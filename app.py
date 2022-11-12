7539c4846640509c77f9595db44e6b0dae452709
bereket <bereketsiraw123@gmail.com>
2022-11-12 15:09:10 +0400

    my_posts.html

--- app.py
+++ app.py
@@ -2,7 +2,18 @@ import flask
 from flask import jsonify, render_template, request
 
 app = flask.Flask(__name__)
-DOMAIN = "dealAD"
+
+@app.route('/', methods=['GET'])
+def login():
+    return render_template("login.html")
+
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template("register.html")
+
+@app.route('/my_posts', methods=['GET'])
+def myPosts():
+    return render_template("posts/my_posts.html")
 
 @app.route('/create', methods=['GET'])
 def create_post():
@@ -14,6 +25,7 @@ def create_sell():
 @app.route('/create/sell', methods=['GET', 'POST'])
 def create_sell_action():
     return render_template("posts/create_sell.html")
+
 @app.route('/create/buy', methods=['GET'])
 def create_buy():
     return render_template("posts/create_buy.html")
@@ -25,15 +37,6 @@ def create_buy_action():
     print(amount,rate)
     return render_template("posts/create_buy.html")
 
-# @app.route(f'{DOMAIN}/createSell/', methods=['GET'])
-# def getMembers(request):
-#     return render_template("createsellPost.html", request)
 
-# @app.route('/api/v1/register', methods=['POST'])
-# def register(request):
-#     validate.validateInput(request)
-
-app.run(debug=True)
-
-
+app.run(debug=True, port=80)
 
