c136dfc4e015bf26a55e54baef8fd2f79ee0f76b
Absera <abseratemesgen@gmai.com>
2022-11-12 13:07:11 +0400

    heyyy

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ validate = __import__("backend/validate")
 app = flask.Flask(__name__)
 
 # [Begin] Absera
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template("register.html")
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template()
+    return render_template("register.html")
 # [End] Absera
 
 @app.route('/api/v1/users', methods=['GET', 'POST'])
