3caff9489fa925657cbb977a1b735d61637fb37f
bereket <bereketsiraw123@gmail.com>
2022-11-12 15:24:40 +0400

    database 1st

--- app.py
+++ app.py
@@ -33,7 +33,15 @@ create_buy_table_query = """ CREATE TABLE IF NOT EXISTS buy_posts (
                                         rate integer
                                     ); """
 
+create_users_table_query = """ CREATE TABLE IF NOT EXISTS users (
+                                        userid integer PRIMARY KEY AUTOINCREMENT,
+                                        firstname text,
+                                        password text,
+                                        netid text
+                                    ); """
 
+
+database_cursor.execute(create_users_table_query)
 database_cursor.execute(create_buy_table_query)
 database_cursor.execute(create_sell_table_query)
 
